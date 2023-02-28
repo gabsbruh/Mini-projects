75f3a93e7a1549a47227f5daf3f54d448d9207d3
Gabriel <gabsdidit@icloud.com>
2023-02-28 10:22:40 +0100

    Implemented main function

--- 14. higher-lower-game/main.py
+++ 14. higher-lower-game/main.py
@@ -11,6 +11,7 @@ def random_accounts():
     account = random.choice(accounts_data.data)   
     return account
 
+
 def which_bigger(account_1, account_2):
     """Funtion checking which account is bigger basing on the followe_count value. 
 
@@ -21,13 +22,15 @@ def which_bigger(account_1, account_2):
         return 1
     else:
         return 2
-    
+
+
 def print_accounts(account_1 = {}, account_2 = {}):
     print("Which account has more followers on Instagram?\n")
     print(f"{account_1['name']}, {account_1['description']}, from {account_1['country']} ")
     print("\tvs\t")
     print(f"{account_2['name']}, {account_2['description']}, from {account_2['country']} ")
-    
+
+
 def program():
     print("\t*** HIGHER-LOWER GAME ***\t")
     again = True
@@ -37,7 +40,7 @@ def program():
         if play == 'y':
             score = 0
             is_right = True
-            while is_right == True:
+            while is_right:
                 account_1 = random_accounts()
                 account_2 = random_accounts()
                 if account_1 == account_2 or account_1["follower_count"] == account_2["follower_count"]:
@@ -48,7 +51,7 @@ def program():
                 choice = int(input(f"\nType '1' if you choose {account_1['name']}, or '2' if you choose {account_2['name']}: "))
                 os.system('cls')
                 if choice == which_bigger(account_1, account_2):
-                    ### score up
+                    # score up
                     score += 1
                     print("Good choice! Your actual score is " + str(score))
                     is_right = True
@@ -61,6 +64,12 @@ def program():
         else:
             print("Wrong input!")
             program()
-        
+
+
+def main():
+    program()
+
+
 # RUN
-program()
+if __name__ == '__main__':
+    main()
