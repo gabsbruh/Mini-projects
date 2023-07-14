242c17ee50f03d62c4089e378e41b16871c92123
Gabriel <gabsdidit@icloud.com>
2023-07-14 02:47:17 +0200

    easier coffe_machine but in OOP

--- 16. OOP-coffee-machine/main.py
+++ 16. OOP-coffee-machine/main.py
@@ -1,1 +1,28 @@
-# few changres
+from menu import Menu
+from money_machine import MoneyMachine
+from coffee_maker import CoffeeMaker
+
+menu = Menu()
+maker = CoffeeMaker()
+money = MoneyMachine()
+
+order_name = input('What do you want to drink? (espresso/latte/cappucino):\n')
+if order_name is 'report':
+    maker.report()
+else:
+    item = menu.find_drink(order_name)
+if not maker.is_resource_sufficient(item):
+    print("Insufficent resources!")
+print( f'Put ${item.cost} to the machine:')
+money.report()
+if money.make_payment(item.cost):
+    money.report()
+    maker.make_coffee(item)
+else:
+    print("Insufficent funds!")
+
+    
+
+    
+
+
