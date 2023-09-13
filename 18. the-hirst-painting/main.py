a1257502e96f4acefe00be5478fcb56de5fc2e59
Gabriel <gabsdidit@icloud.com>
2023-09-13 20:40:09 +0200

    arrangements in finished code

--- 18. the-hirst-painting/main.py
+++ 18. the-hirst-painting/main.py
@@ -3,16 +3,48 @@ import turtle as t
 import random
 
 
-# finding colours from painting
-colours = cg.extract('the_hirst_painting.jpg', 100)
-in_rgb = []
+def change_color(turtle):
+    turtle.color(random.choice(in_rgb))
 
-for colr in colours:
-    r = colr.rgb.r
-    g = colr.rgb.g
-    b = colr.rgb.b
-    in_rgb.append((r,g,b))
 
+def main():
+    # finding colours from painting
+    colours = cg.extract('the_hirst_painting.jpg', 100)
+    in_rgb = []
+
+    for colr in colours:
+        r = colr.rgb.r
+        g = colr.rgb.g
+        b = colr.rgb.b
+        in_rgb.append((r,g,b))
+        
+    # code to run the turtle with drawing dots        
+        #adjustments
+    screen = t.Screen()
+    screen.colormode(255)
+    spark = t.Turtle()
+    spark.shape('arrow')
+    spark.speed(11)
+
+        #action
+    spark.pu()
+    spark.setpos(-425, -425) # setting down-left to start an algorithm from the corner
+    for j in range(1,11):
+        for i in range(10):
+            change_color(spark)
+            spark.pd()
+            spark.begin_fill()
+            spark.circle(20)
+            spark.end_fill()
+            spark.pu()
+            spark.forward(90)
+        spark.setpos(-425, (-425) + 90*j)  
+    screen.exitonclick()
+    
+    
+if __name__ == '__main__':
+    main() 
+        
 # in rgb:
 # [(240, 246, 243), (132, 166, 205), (221, 148, 106), 
 # (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), 
@@ -22,32 +54,3 @@ for colr in colours:
 # (170, 188, 221), (57, 51, 48), (184, 103, 113), (32, 60, 109),
 # (105, 126, 159), (175, 200, 188), (34, 151, 210), (65, 66, 56),
 # (106, 140, 124), (153, 202, 227), (48, 69, 71), (131, 128, 121)]
-
-# code to run the turtle with drawing dots
-
-def change_color(turtle):
-    turtle.color(random.choice(in_rgb))
-
-#adjustments
-screen = t.Screen()
-screen.colormode(255)
-spark = t.Turtle()
-spark.shape('arrow')
-spark.speed(11)
-
-spark.pu()
-spark.setpos(-425, -425) # setting down-left to start an algorithm from the corner
-for j in range(1,11):
-    for i in range(10):
-        change_color(spark)
-        spark.pd()
-        spark.begin_fill()
-        spark.circle(20)
-        spark.end_fill()
-        spark.pu()
-        spark.forward(90)
-    spark.setpos(-425, (-425) + 90*j)  
-       
-screen.exitonclick()
-    
-        
