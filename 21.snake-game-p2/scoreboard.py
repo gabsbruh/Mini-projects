44d05655d8043decded72bf4b9733e7407a11e22
Gabriel <gabsdidit@icloud.com>
2024-02-02 16:57:50 +0100

    added new high score tool

--- 21.snake-game-p2/scoreboard.py
+++ 21.snake-game-p2/scoreboard.py
@@ -7,25 +7,45 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('score.txt', 'r') as high_score:
+            self.high_score = int(high_score.read())
         self.color('white')            
         self.pu()
         self.goto(0, 270)
-        self.write_score()
+        self.print_score()
         self.ht()     
         
-    def write_score(self):
+    def print_score(self):
+        self.clear()
         self.write(f'Score: {self.score}', align=ALIGNMENT, font = FONT)
     
     def score_up(self):
         self.score += 1
         self.clear()
-        self.write_score()
+        self.print_score()
+        
+    def new_best(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('score.txt', 'w') as high_score:
+                high_score.write(str(self.high_score))
+            return True
+        else:
+            return False
         
     def game_over(self):
-        self.goto(0,0)
-        self.write('GAME OVER!', align=ALIGNMENT, font = GAME_OVER_FONT) 
-        self.goto(0,-30)  
-        self.write(f'Your Score: {self.score}', align=ALIGNMENT, font = FONT)
+        if not self.new_best():
+            self.goto(0,0)
+            self.write('GAME OVER!', align=ALIGNMENT, font = GAME_OVER_FONT) 
+            self.goto(0,-30)  
+            self.write(f'Your Score: {self.score}', align=ALIGNMENT, font = FONT)
+            self.goto(0,-60)  
+            self.write(f'Highscore: {self.high_score}', align=ALIGNMENT, font = FONT)
+        else:
+            self.goto(0,0)
+            self.write('NEW BEST!', align=ALIGNMENT, font = GAME_OVER_FONT) 
+            self.goto(0,-30)  
+            self.write(f'Your Score: {self.score}', align=ALIGNMENT, font = FONT)                    
     
     def current_score(self):
         return self.score
--- 23.turtle-crossing-game/scoreboard.py
+++ 23.turtle-crossing-game/scoreboard.py
@@ -8,24 +8,56 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.level = 1
-        self.color('black')            
+        self.color('black')    
+        with open('score.txt', 'r') as file:
+            read = file.read()
+            hl, hp = read.split()
+            self.high_score = [int(hl), int(hp)]
         self.pu()
         self.goto(-220, 260)
-        self.write_score()
-        self.ht()     
+        self.print_score()
+        self.ht()
+       
+    def set_high_score(self, progress):
+        if self.level > self.high_score[0]:
+            self.high_score = [str(self.level), str(progress)]
+            with open('score.txt', 'w') as file:
+                file.write(str(self.high_score[0]) + ' ' + self.high_score[1])
+            return True
+        else:
+            return False
+        
+        if self.level == self.high_score[0]:
+            if progress > self.high_score[1]:
+                self.high_score = [str(self.level), str(progress)]
+                with open('score.txt', 'w') as file:
+                    file.write(str(self.high_score[0]) + ' ' + self.high_score[1])
+                return True
+        else:
+            return False
            
-    def write_score(self):
+    def print_score(self):
         self.write(f'Level: {self.level}/30', align=ALIGNMENT, font = FONT)    
         
     def next_level(self):
         self.level += 1
         self.clear()
-        self.write_score()
+        self.print_score()
 
     def game_over(self, progress):
-        self.goto(0,0)
-        self.write('GAME OVER!', align=ALIGNMENT, font = GAME_OVER_FONT) 
-        self.goto(0,-30)  
-        self.write(f'Reached level: {self.level}', align=ALIGNMENT, font = FONT)
-        self.goto(0,-60)  
-        self.write(f'Progress: {progress}%', align=ALIGNMENT, font = FONT)
+        if not self.set_high_score(progress):
+            self.goto(0,0)
+            self.write('GAME OVER!', align=ALIGNMENT, font = GAME_OVER_FONT) 
+            self.goto(0,-30)  
+            self.write(f'Reached level: {self.level}', align=ALIGNMENT, font = FONT)
+            self.goto(0,-60)  
+            self.write(f'Progress: {progress}%', align=ALIGNMENT, font = FONT)
+            self.goto(0,-100)          
+            self.write(f'High Score: level {self.high_score[0]}, {self.high_score[1]}%', align=ALIGNMENT, font = FONT)
+        else:
+            self.goto(0,0)
+            self.write('NEW BEST!', align=ALIGNMENT, font = GAME_OVER_FONT) 
+            self.goto(0,-30)  
+            self.write(f'Reached level: {self.level}', align=ALIGNMENT, font = FONT)
+            self.goto(0,-60)  
+            self.write(f'Progress: {progress}%', align=ALIGNMENT, font = FONT)
