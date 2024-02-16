a15dda9d503f87602a31cb7dfdc035fffdd5fd02
Gabriel <gabsdidit@icloud.com>
2024-02-16 13:53:31 +0100

    catch input exceptions

--- 26.NATO-phonetic-alphabet/main.py
+++ 26.NATO-phonetic-alphabet/main.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('nato_phonetic_alphabet.csv') # read from csv as pd.df
 df_dict = {row[0]: row[1] for (index, row) in df.iterrows()} # change format from pd.df to a dict
-print([df_dict[key.upper()] for key in input('Write a word to spell:')]) # print list of words literating from user's input
+
+# while loop for handling exception with wrong input given
+exception_occured = True
+while exception_occured:
+    try:
+        print([df_dict[key.upper()] for key in input('Write a word to spell:')]) # print list of words literating from user's input
+        exception_occured = False
+    except KeyError:
+        print('Wrong input were given. Only letters are allowed.')
+        
