1474b27afe11f0ac635f1f3e478bd63234fa7cf3
Gabriel <gabsdidit@icloud.com>
2024-03-02 23:59:12 +0100

    initialize sms stock strading

---
+++ 36.SMS-stock-trading-alerts/main.py
@@ -0,0 +1,39 @@
+import requests
+
+## TODO 1: Use https://www.alphavantage.co
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+api_key = "SHPBSG8PA78PHTXO"
+stock = "TSLA"
+company_name = "Tesla Inc"
+
+url = 'https://www.alphavantage.co/query'
+params = {
+    "function": "TIME_SERIES_INTRADAY",
+    "symbol": stock,
+    "interval": "5min",
+    "apikey": api_key,
+}
+
+r = requests.get(url=url, params=params)
+data = r.json()
+
+print(data)
+
+## TODO 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+## TODO 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
