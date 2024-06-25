67a5a708c03649cacfd0e25b886cd39e02b7aa46
Gabriel <gabsdidit@icloud.com>
2024-06-25 12:59:33 +0200

    correct endpoints

--- 39.flight-deals-alert/constants.py
+++ 39.flight-deals-alert/constants.py
@@ -10,7 +10,7 @@ AMADEUS_API_KEY = os.environ.get("AMADEUS_API_KEY")
 AMADEUS_API_SECRET = os.environ.get("AMADEUS_API_SECRET")
 AMADEUS_URL_IATA_CODE = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
 AMADEUS_URL_NEW_TOKEN = "https://test.api.amadeus.com/v1/security/oauth2/token"
-AMADEUS_URL_FLIGHTS = "https://test.api.amadeus.com/v2/shopping-flight-offers"
+AMADEUS_URL_FLIGHTS = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 # specifications for flights
 ADULTS = 2
