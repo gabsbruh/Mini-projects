399fd9d53441077dab40d6dcf052eb7898f4f370
Gabriel <gabsdidit@icloud.com>
2024-07-03 20:18:12 +0200

    fill info about cheapest flight

--- 39.flight-deals-alert/data_manager.py
+++ 39.flight-deals-alert/data_manager.py
@@ -4,15 +4,24 @@ import constants as c
 class DataManager:
     """This class is responsible for talking to the Google Sheet."""
     def __init__(self):
-        self.auth = c.AUTH_HEADER
-        self.url = c.SHEETY_URL
+        self._auth = c.AUTH_HEADER
+        self._url = c.SHEETY_URL
         
-    def fill_sheet(self, city_id, iata_code):
+    def get_data(self) -> dict:
+        """Get data from sheet.
+        
+        Returns:
+            dict: json with all data.
+        """
+        sheet_data = requests.get(url=self._url, headers=self._auth)
+        return sheet_data.json()
+        
+    def fill_iata_codes(self, city_id, iata_code):
         """fill iata code inside sheet
 
         Args:
             city_id (int): number of row inside spreadsheet in which iata code will be changed
-            iata_code (string): iata_code
+            iata_code (str): iata_code
         """
         put_params = {
             "price":{
@@ -20,17 +29,31 @@ class DataManager:
             }         
         }
         try:
-            response = requests.put(url=f"{self.url}/{city_id}", json=put_params, headers=self.auth)
+            response = requests.put(url=f"{self._url}/{city_id}", json=put_params, headers=self._auth)
         except response.status_code != 200:
-            print('There was a problem with retrieving data.')
+            print('There was a problem with filling iata codes.')
             print(response.text)
     
-    def get_data(self) -> dict:
-        """Get data from sheet.
-        
-        Returns:
-            dict: json with all data.
+    def fill_cheapest_info(self, city_id, info):
+        """fill info about cheapest flight on specified destination
+
+        Args:
+            city_id (int): number of row inside spreadsheet in which iata code will be changed
+            info (dict): info about cheapest flight, including dates, pricing, carrier
         """
-        sheet_data = requests.get(url=self.url, headers=self.auth)
-        return sheet_data.json()
-        
+        put_params = {
+            "price":{
+                "cheapest": info['price'],
+                "departureDate": info['departureDate'],
+                "arrivalDate": info['arrivalDate'],
+                "departureCarrier": info['departureCarrier'],
+                "arrivalCarrier": info['arrivalCarrier']
+            }         
+        }
+        try:
+            response = requests.put(url=f"{self._url}/{city_id}", json=put_params, headers=self._auth)
+        except response.status_code != 200:
+            print('There was a problem with filling info about flights.')
+            print(response.text)
+        else:
+            print('Filling sheet succeded.')
