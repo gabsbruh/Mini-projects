e162da3cf3af8b19001b33faf17ecdb2100d7bae
Gabriel <gabsdidit@icloud.com>
2024-07-03 20:20:14 +0200

    reached full program cycle

--- 39.flight-deals-alert/main.py
+++ 39.flight-deals-alert/main.py
@@ -5,7 +5,6 @@ import constants as c
 from data_manager import DataManager
 from flight_data import FlightData
 from flight_search import FlightSearch
-from notification_manager import NotificationManager
 
 def main():
     # get data from sheet
@@ -19,19 +18,19 @@ def main():
             city = elem["city"]
             id = elem["id"]
             iata_code = flight_search.get_iata_code(city_name=city)
-            data_manager.fill_sheet(city_id=id, iata_code=iata_code)
+            data_manager.fill_iata_codes(city_id=id, iata_code=iata_code)
 
+    # get refreshed data from sheet
+    sheet_data = data_manager.get_data()
 
+    # pass info from sheet data to the flight_data to look for cheapest flights
+    for destination in sheet_data["prices"]:
+        
+        flight_data = FlightData(max_price=destination['maxPrice'], 
+                                 destination_code=destination['iataCode'])
+        info_cheapest = flight_data.get_cheapest_flight(min_days=destination['minimumDays'],
+                                        max_days=destination['maximumDays'])
+        data_manager.fill_cheapest_info(city_id=destination['id'], info=info_cheapest)
+    
 if __name__ == "__main__":
     main()
-
-
-### XXX:
-""" Aktualnie apka znajduje najtanszy lot w podanym przedziale dat, na podstawie wyszukiwania
-dla jednego destination (flight_data i tam jest instancja dla PAR i   price 2000)
-
-Rate limit wciaz nie jest dobrany, srednio raz na 10 calli wyskakuje proglem (mimo ze jest ustawiony ponizej
-wymaganego na amadeus api, moze trzeba zmniejszyc period do 100ms i 1 call)
-
-kolejny krok to zautomatyzowanie wyszukiwania najtanszych lotow
-"""
