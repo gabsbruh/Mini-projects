4f890980b0a4c0db7b82f82314c271ce3f17550d
Gabriel <gabsdidit@icloud.com>
2024-07-04 13:01:11 +0200

    add email sender with offers

--- 39.flight-deals-alert/main.py
+++ 39.flight-deals-alert/main.py
@@ -5,6 +5,7 @@ import constants as c
 from data_manager import DataManager
 from flight_data import FlightData
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 def main():
     # get data from sheet
@@ -20,7 +21,7 @@ def main():
             iata_code = flight_search.get_iata_code(city_name=city)
             data_manager.fill_iata_codes(city_id=id, iata_code=iata_code)
 
-    # get refreshed data from sheet
+    # # get refreshed data from sheet
     sheet_data = data_manager.get_data()
 
     # pass info from sheet data to the flight_data to look for cheapest flights
@@ -30,7 +31,15 @@ def main():
                                  destination_code=destination['iataCode'])
         info_cheapest = flight_data.get_cheapest_flight(min_days=destination['minimumDays'],
                                         max_days=destination['maximumDays'])
+        # now fill sheet with best offers on each destination
         data_manager.fill_cheapest_info(city_id=destination['id'], info=info_cheapest)
+    
+    # get refreshed data from sheet
+    sheet_data = data_manager.get_data()   
+    
+    # send an email to all application users about offers
+    notify = NotificationManager(users_list=data_manager.users, info_about_flights=sheet_data)
+    notify.send_emails()
     
 if __name__ == "__main__":
     main()
