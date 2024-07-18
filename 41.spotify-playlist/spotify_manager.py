854a2b55e961d38ed1374f34d9c312b06ca113b3
Gabriel <gabsdidit@icloud.com>
2024-07-18 16:24:17 +0200

    search uri songs

--- 41.spotify-playlist/spotify_manager.py
+++ 41.spotify-playlist/spotify_manager.py
@@ -10,6 +10,7 @@ class SpotifyManager:
     """
     def __init__(self):
         pass        self.sp = spotipy.Spotify(
+        self.sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
                 client_id=c.SPOTIFY_CLIENT_ID,
                 client_secret=c.SPOTIFY_CLIENT_SECRET,
@@ -20,4 +21,15 @@ class SpotifyManager:
             )
         )
         self.user_id = self.sp.current_user()["id"]
+    
+    def search_for_song(self, song_name, song_author, year):
+        # I withdrew searching by arist due to the plenty of unsuccessful searches
+        searching_result = self.sp.search(q=f"track:{song_name} year:{year}", type="track")
+        try:
+            uri = searching_result["tracks"]["items"][0]["uri"]
+        except IndexError:
+            print(f"'{song_name}' by '{song_author}' doesn't exist on Spotify. It will be skipped in further process.")
+            return None
+        else:
+            return uri
     
