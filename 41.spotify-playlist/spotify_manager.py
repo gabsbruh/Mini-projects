5f2f4ab9b548d1ee8026a7e0b917f3a3029e0f03
Gabriel <gabsdidit@icloud.com>
2024-07-18 16:23:55 +0200

    composition of spotipy module

--- 41.spotify-playlist/spotify_manager.py
+++ 41.spotify-playlist/spotify_manager.py
@@ -1,7 +1,23 @@
+import requests
+import constants as c
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import base64
 
-
-class SpotifyManager():
+# with spotipy create its composition in own class
+class SpotifyManager:
     """Correspond with Spotify API
     """
     def __init__(self):
-        pass
+        pass        self.sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=c.SPOTIFY_CLIENT_ID,
+                client_secret=c.SPOTIFY_CLIENT_SECRET,
+                redirect_uri="https://example.org/callback",
+                scope="playlist-modify-private ugc-image-upload",
+                cache_path="token.txt",
+                show_dialog=True,
+            )
+        )
+        self.user_id = self.sp.current_user()["id"]
+    
