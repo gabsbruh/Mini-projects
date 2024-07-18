2f8046ee85bc3b906e4aadb0904b530ccc712ccf
Gabriel <gabsdidit@icloud.com>
2024-07-18 16:25:22 +0200

    upload cover in base64 format

--- 41.spotify-playlist/spotify_manager.py
+++ 41.spotify-playlist/spotify_manager.py
@@ -50,6 +50,19 @@ class SpotifyManager:
             raise Exception("Playlist was not created. See details in json response")
             print(new_playlist)
         
+    def upload_playlist_cover_image(self, cover_image_path: str):
+        def jpg_to_base64_format(image_path):
+            # encode image from jpg to base64 stream format
+                with open(image_path, 'rb') as image_file:
+                    image_data = image_file.read()
+                    base64_encoded_data = base64.b64encode(image_data)
+                    base64_string = base64_encoded_data.decode('utf-8')
+                    return base64_string
+        
+        cover_image_encoded = jpg_to_base64_format(cover_image_path)
+        self.sp.playlist_upload_cover_image(playlist_id=self.playlist_id,
+                                            image_b64=cover_image_encoded)
+    
     def add_tracks(self, tracks_uri: list):  
         self.sp.user_playlist_add_tracks(user=self.user_id, 
                                          playlist_id=self.playlist_id, 
