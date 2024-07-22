9a5bc73aa9415c53bbb3dea84ee0ab0c2125ee2c
Gabriel <gabsdidit@icloud.com>
2024-07-22 17:58:32 +0200

    preventing bots

---
+++ 42.live-site-price-tracker/my_header.py
@@ -0,0 +1,16 @@
+full_header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8", 
+    "Accept-Encoding": "gzip, deflate, br, zstd", 
+    "Accept-Language": "pl-PL,pl", 
+    "Priority": "u=0, i", 
+    "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Brave\";v=\"126\"", 
+    "Sec-Ch-Ua-Mobile": "?0", 
+    "Sec-Ch-Ua-Platform": "\"Windows\"", 
+    "Sec-Fetch-Dest": "document", 
+    "Sec-Fetch-Mode": "navigate", 
+    "Sec-Fetch-Site": "cross-site", 
+    "Sec-Fetch-User": "?1", 
+    "Sec-Gpc": "1", 
+    "Upgrade-Insecure-Requests": "1", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36", 
+    }
