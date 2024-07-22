d00bdd95ef8d4c8d23dd3e814912bd3c9da5536c
Gabriel <gabsdidit@icloud.com>
2024-07-22 17:59:29 +0200

    omit CAPTCHA by mutating request

---
+++ 42.live-site-price-tracker/trick_live_site.py
@@ -0,0 +1,12 @@
+import random
+
+class HeaderSwitch:
+    def __init__(self, orginal_header: dict):
+        self.orginal_header = orginal_header
+    
+    def change_header(self):
+        no_random_variables = random.randint(4, 14)
+        random_keys = random.sample(list(self.orginal_header.keys()), no_random_variables)
+        new_header = {key: self.orginal_header[key] for key in random_keys}
+        return new_header
+            
