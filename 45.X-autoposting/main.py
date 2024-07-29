6d5f4b2e6112a48e1dec75c7c0a9eac3eeef379d
Gabriel <gabsdidit@icloud.com>
2024-07-30 00:18:48 +0200

    complete workflow

--- 45.X-autoposting/main.py
+++ 45.X-autoposting/main.py
@@ -3,7 +3,12 @@ from x_manager import XManager
 
 
 def main():
-    pass
+    is_driver = InternetSpeed()
+    is_driver.measure_speed()
+    feedback = iss.report() # check if conditions are met
+    if not feedback: # if conditions fulfilled, won't be executed
+        x_driver = XManager(upload_speed=is_driver.upload, download_speed=is_driver.download)
+        x_driver.post_complaint() 
 
 
 if __name__ == "__main__":
